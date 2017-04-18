9b5377cc519857d04f4b6791055317dccc293d55
dongzhuoyao <929109550@qq.com>
2017-04-19 04:06:26 +0800

    t

--- run_train_i23d.sh
+++ run_train_i23d.sh
@@ -1,2 +1,2 @@
 CUDA_VISIBLE_DEVICES=0  python train.py --data_dir /cdata/hut/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012 \
---restore_from model.ckpt-init
+--restore_from ./model.ckpt-init
