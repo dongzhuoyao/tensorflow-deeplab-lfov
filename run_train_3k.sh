b5870af39425348c116c30a62573204f2193e1fe
dongzhuoyao <929109550@qq.com>
2017-04-19 04:05:14 +0800

    t

--- run_train_3k.sh
+++ run_train_3k.sh
@@ -1,1 +1,2 @@
-python train.py --data_dir /home/hutao1/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012
+CUDA_VISIBLE_DEVICES=1  python train.py --data_dir /home/hutao1/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012 \
+--restore_from model.ckpt-init
--- run_train_i23d.sh
+++ run_train_i23d.sh
@@ -1,1 +1,2 @@
-python train.py --data_dir /cdata/hut/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012
+CUDA_VISIBLE_DEVICES=0  python train.py --data_dir /cdata/hut/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012 \
+--restore_from model.ckpt-init
