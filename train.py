e25a8f809e6ac25662786e12e591d490f9d7f5f2
dongzhuoyao <929109550@qq.com>
2017-04-23 02:06:50 +0800

    t

--- train.py
+++ train.py
@@ -171,7 +171,7 @@ def main():
         tf.summary.image("predict_2", pre_upscaled_2_converted)
         tf.summary.image("predict_3", pre_upscaled_3_converted)
         tf.summary.image('total',
-                         tf.concat([convert(image_batch), convert(label_batch), pre_upscaled_1_converted,pre_upscaled_2_converted,pre_upscaled_3_converted], 2),
+                         tf.concat([convert(image_batch), convert(decode_labels(label_batch)), convert(decode_labels(pre_upscaled_1)),convert(decode_labels(pre_upscaled_2)),convert(decode_labels(pre_upscaled_3))], 2),
                          max_outputs=4)
 
     merged_summary_op = tf.summary.merge_all()
