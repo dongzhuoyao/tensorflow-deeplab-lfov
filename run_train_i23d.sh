a3cff190c417c52dc82756ae978a6b8dcd0276a8
dongzhuoyao <929109550@qq.com>
2017-04-23 22:17:29 +0800

    t

--- run_train_i23d.sh
+++ run_train_i23d.sh
@@ -1,3 +1,4 @@
 git pull && python train.py --data_dir /cdata/hut/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012 \
 --restore_from  ./model.ckpt-pretrained \
---save_pred_every 50
+--save_pred_every 50 \
+&& tensorboard --logdir ./summary --port 7777
