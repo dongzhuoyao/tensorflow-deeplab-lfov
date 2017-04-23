9dbf1e40e6f1c91dbe6ff9c1b5a032ee1e21dddc
dongzhuoyao <929109550@qq.com>
2017-04-23 21:57:18 +0800

    t

--- run_train_i23d.sh
+++ run_train_i23d.sh
@@ -1,3 +1,3 @@
 python train.py --data_dir /cdata/hut/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012 \
---restore_from  ./model.ckpt-init \
+--restore_from  ./model.ckpt-pretrained \
 --save_pred_every 50
