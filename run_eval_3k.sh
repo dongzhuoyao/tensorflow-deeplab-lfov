371adfb13b98ae005fc00984801b89332e56bdbd
dongzhuoyao <929109550@qq.com>
2017-04-23 13:20:02 +0800

    t

--- run_eval_3k.sh
+++ run_eval_3k.sh
@@ -1,2 +1,2 @@
 python evaluate.py --data_dir /home/hutao1/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012 \
---restore_from  ./model.ckpt-pretrained
+--restore_from  ./snapshots/model.ckpt-9000
