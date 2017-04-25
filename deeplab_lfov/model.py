1ede578c782252ffc89baebd387154216f7d6486
dongzhuoyao <929109550@qq.com>
2017-04-26 00:40:12 +0800

    t

--- deeplab_lfov/model.py
+++ deeplab_lfov/model.py
@@ -111,7 +111,7 @@ class DeepLabLFOVModel(object):
         
         v_idx = 0 # Index variable.
         is_deal_first_layer = 0
-        aggregated_feat = []
+        aggregated_feat = None
 
 
         # Last block is the classification layer.
@@ -208,7 +208,7 @@ class DeepLabLFOVModel(object):
           A downsampled segmentation mask.
         """
         current = input_batch
-        aggregated_feat = []
+        aggregated_feat = None
 
         v_idx = 0  # Index variable.
         #is_deal_first_layer = 0
