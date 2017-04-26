0acec6298ebd26add844810ba5c84d1a3a634b9a
dongzhuoyao <929109550@qq.com>
2017-04-26 20:50:41 +0800

    t

--- run_train_ml2.sh
+++ run_train_ml2.sh
@@ -1,3 +1,3 @@
-git pull && python train.py --data_dir /media/F/hutao/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012  \
+CUDA_VISIBLE_DEVICES=1  git pull && python train.py --data_dir /media/F/hutao/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012  \
 --restore_from  ./model.ckpt-pretrained \
 --save_pred_every 50
