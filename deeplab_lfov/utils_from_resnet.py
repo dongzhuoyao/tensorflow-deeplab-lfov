8b9ceaf9d9e274498efe88b37301fe9c7555ad30
dongzhuoyao <929109550@qq.com>
2017-04-30 19:03:40 +0800

    t

--- deeplab_lfov/utils_from_resnet.py
+++ deeplab_lfov/utils_from_resnet.py
@@ -119,8 +119,8 @@ def single_channel_process(imgs, num_images):
     outputs = np.zeros((num_images, h, w, 3), dtype=np.uint8)
     for i in range(num_images):
         tmp = imgs[i].flatten()
-        print tmp
-        bins = np.array([0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0])
+        #bins = np.array([0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0])
+        bins = np.array([0.0, 1,10,30,100,200])
         tmp = np.digitize(tmp, bins)
         tmp = tmp*255.0/tmp.max()
         tmp = tmp.reshape((h,w)).astype(np.uint8)
