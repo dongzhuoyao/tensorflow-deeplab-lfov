bfbf3ef2151c991e3ee71927116e78ead83cd2d1
dongzhuoyao <929109550@qq.com>
2017-04-30 23:25:03 +0800

    t

--- train.py
+++ train.py
@@ -138,7 +138,7 @@ def main():
         _,hed_total_cost,predict_4d_label,cam_pre,cam_gt,confidence_map,predict_4d = net.loss(image_batch, label_batch,weight_decay=weight_decay)
 
     confidence_map_print = tf.Print(confidence_map, [tf.nn.top_k(confidence_map,k=1)[0]],'reduce_max(confidence_map) = ', summarize=20, first_n=100)
-    cam_gt_print = tf.Print(cam_gt, [tf.nn.top_k(predict_4d,K=10)[0]], 'top_k(predict_4d) = ',summarize=20, first_n=100)
+    cam_gt_print = tf.Print(cam_gt, [tf.nn.top_k(predict_4d,k=10)[0]], 'top_k(predict_4d) = ',summarize=20, first_n=100)
 
     learning_rate = tf.placeholder(tf.float32, shape=[])
     optimiser = tf.train.MomentumOptimizer(learning_rate=learning_rate,momentum=0.9)
