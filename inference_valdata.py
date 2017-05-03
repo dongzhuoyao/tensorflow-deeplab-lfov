b1e9614e9d0aebdf511db6f8499eb982ace65c6e
dongzhuoyao <929109550@qq.com>
2017-05-03 15:40:09 +0800

    t

--- inference_valdata.py
+++ inference_valdata.py
@@ -116,8 +116,9 @@ def main():
         pred_result = np.array(pred_result)[0,0, :, :, 0]
         print("np.array(pred_result) shape: {}".format(pred_result.shape))
 
-        wrong = np.not_equal(pred_result.astype(np.uint8),label.astype(np.uint8))
-        print(wrong)
+        wrong = np.not_equal(pred_result.astype(np.uint8),label.astype(np.uint8))*255
+        np.expand_dims(wrong,axis=2)
+
 
 
 
