0221d96448a390bf21d03a9406c20fcb2c229266
dongzhuoyao <929109550@qq.com>
2017-05-03 18:25:39 +0800

    t

---
+++ inference_voc_train_ml2.sh
@@ -0,0 +1,4 @@
+python inference_valdata.py  --img_path /media/F/hutao/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012 \
+  --txt_path ./dataset/train.txt \
+  --model_weights ./model.ckpt-pretrained \
+  --save_dir ./voc_train_result
