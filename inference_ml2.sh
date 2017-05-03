d1d2ef82f42d1bd048f5fbfea875ed20195cacbe
dongzhuoyao <929109550@qq.com>
2017-05-03 11:03:36 +0800

    t

--- inference_ml2.sh
+++ inference_ml2.sh
@@ -1,2 +1,3 @@
-python inference_testdata.py /media/F/hutao/dataset/pascalvoc2012/VOC2012test/VOCdevkit/VOC2012/JPEGImages ./model.ckpt-pretrained \
-./voc_test_result
+python inference_testdata.py  /media/F/hutao/dataset/pascalvoc2012/VOC2012test/VOCdevkit/VOC2012/JPEGImages \
+ ./model.ckpt-pretrained \
+ ./voc_test_result 
