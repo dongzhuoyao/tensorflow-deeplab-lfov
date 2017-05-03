0f5a7b36cec80f37ae77a886dbd35495e3dc79b9
dongzhuoyao <929109550@qq.com>
2017-05-03 16:08:15 +0800

    t

--- deeplab_lfov/model.py
+++ deeplab_lfov/model.py
@@ -182,7 +182,7 @@ class DeepLabLFOVModel(object):
         raw_output = self._create_network(tf.cast(input_batch, tf.float32), keep_prob=tf.constant(1.0))
         raw_output = tf.image.resize_bilinear(raw_output, tf.shape(input_batch)[1:3,])
 
-        confidence = tf.reduce_max(raw_output, dimension=3)
+        confidence = tf.reduce_max(raw_output,  keep_dims=True, axis=3)
         confidence = tf.expand_dims(confidence, dim=3)
 
         raw_output = tf.argmax(raw_output, dimension=3)
