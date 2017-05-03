ded15feec57e87e28116864c59302b8a214649d8
dongzhuoyao <929109550@qq.com>
2017-05-03 13:01:07 +0800

    t

--- inference_valdata.py
+++ inference_valdata.py
@@ -11,11 +11,16 @@ import os
 import sys
 import time
 import glob
+import cv2
 
 from PIL import Image
 
 import tensorflow as tf
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 
 from deeplab_lfov import DeepLabLFOVModel, ImageReader, decode_labels
 
@@ -86,21 +91,41 @@ def main():
 
     f = open(args.txt_path,"r")
     test_img_list = f.readlines()
-    test_img_list = [os.path.join(args.img_path,tmp.split(" ")[0][1:]) for tmp in test_img_list]
-    for current_img_path in test_img_list:
 
-        preds = sess.run([pred],feed_dict={img_path:current_img_path})
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
 
-        msk = decode_labels(np.array(preds)[0, 0, :, :, 0],real=True)
+
+    for i,line in enumerate(test_img_list):
+        tmp = line.split(" ")
+        image_path = os.path.join(args.img_path,tmp[0])
+        label_path = os.path.join(args.img_path,tmp[1])
+        image = cv2.imread(img_path)
+        label = cv2.imread(label_path)
+
+        test_img_list = [os.path.join(args.img_path, tmp.split(" ")[0][1:]) for tmp in test_img_list]
+
+        fig, axes = plt.subplots(1, 3, figsize=(5, 15))
+        preds = sess.run([pred],feed_dict={img_path:image_path})
+
+        msk = decode_labels(np.array(preds)[0, 0, :, :, 0])
         im = Image.fromarray(msk)
-        if not os.path.exists(args.save_dir):
-            os.makedirs(args.save_dir)
-
-        img_name = os.path.basename(current_img_path)
+        img_name = os.path.basename(image_path)
         img_name = img_name.replace("jpg", "png")
-        im.save(os.path.join(args.save_dir,img_name))
 
+        #im.save(os.path.join(args.save_dir,img_name))
+
+        axes.flat[i * 3].set_title('data')
+        axes.flat[i * 3].imshow(image).astype(np.uint8)
+
+        axes.flat[i * 3 + 1].set_title('mask')
+        axes.flat[i * 3 + 1].imshow(label)
+
+        axes.flat[i * 3 + 2].set_title('pred')
+        axes.flat[i * 3 + 2].imshow(decode_labels(preds[i, :, :, 0]))
         print('The output file has been saved to {}'.format(os.path.join(args.save_dir,img_name)))
+        plt.savefig(os.path.join(args.save_dir,img_name))
+        plt.close(fig)
 
     
 if __name__ == '__main__':
