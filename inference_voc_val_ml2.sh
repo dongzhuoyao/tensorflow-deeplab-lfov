d9d613ea203dc664150323228f4b72db8119d8ff
dongzhuoyao <929109550@qq.com>
2017-05-03 12:45:44 +0800

    t

--- inference_voc_val_ml2.sh
+++ inference_voc_val_ml2.sh
@@ -1,4 +1,4 @@
-python inference_valdata.py  --img_path /media/F/hutao/dataset/pascalvoc2012/VOC2012test/VOCdevkit/VOC2012/JPEGImages \
+python inference_valdata.py  --img_path /media/F/hutao/dataset/pascalvoc2012/VOC2012trainval/VOCdevkit/VOC2012 \
   --txt_path ./dataset/val.txt \
   --model_weights ./model.ckpt-pretrained \
   --save_dir ./voc_val_result
