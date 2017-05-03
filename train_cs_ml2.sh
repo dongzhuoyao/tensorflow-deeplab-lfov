0d3758af48e34e9e88fbfcc61b66ed4dddf8f39e
dongzhuoyao <929109550@qq.com>
2017-05-03 11:39:24 +0800

    t

--- train_cs_ml2.sh
+++ train_cs_ml2.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=1  python train.py --data_dir /media/F/hutao/dataset/normal_cityscapes  \
 --data_list ./cityscapes_dataset/train.txt \
---input_size 641,641
+--input_size 641,641 \
 --restore_from  ./model.ckpt-pretrained \
 --save_pred_every 500
