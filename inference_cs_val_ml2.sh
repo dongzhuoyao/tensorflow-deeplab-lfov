fd01c5a64d64ef57d75cf41a96ddfcb65ccca309
dongzhuoyao <929109550@qq.com>
2017-05-05 02:09:30 +0800

    t

--- inference_cs_val_ml2.sh
+++ inference_cs_val_ml2.sh
@@ -1,4 +1,4 @@
 python inference_valdata.py  --img_path /media/F/hutao/dataset/normal_cityscapes \
   --txt_path ./cityscapes_dataset/val.txt \
-  --model_weights ./snapshots/model.ckpt-5500 \
+  --model_weights ./snapshots_cs/model.ckpt-5500 \
   --save_dir ./cs_val_result
